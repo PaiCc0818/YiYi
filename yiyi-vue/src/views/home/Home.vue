98cd8df5c5b4fe3c092a6a39887bfa7ec075a5e7
ershuaili <liershuai.cn@gmail.com>
2021-12-28 22:55:21 +0800

    首页框架
    Signed-off-by: ershuaili <liershuai.cn@gmail.com>

--- yiyi-vue/src/views/home/Home.vue
+++ yiyi-vue/src/views/home/Home.vue
@@ -1,11 +1,14 @@
 <template>
-  <!--前台全局组件-->
+  <!--背景-->
+  <div class="background"/>
+  <!--前台全局导航栏组件-->
   <UserNavMenu/>
   <router-view/>
 </template>
 
 <script>
 import UserNavMenu from "@/components/home/UserNavMenu";
+
 export default {
   name: "Home",
   components: {UserNavMenu}
@@ -13,5 +16,11 @@ export default {
 </script>
 
 <style scoped>
-
+.background {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  z-index: -1024;
+  background-color: rgb(224, 231, 234);
+}
 </style>
