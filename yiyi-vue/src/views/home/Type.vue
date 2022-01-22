4e541effcaff2fb601aaf48f2e27dcba42e12302
ershuaili <liershuai.cn@gmail.com>
2022-01-22 23:38:53 +0800

    分类-后端

--- yiyi-vue/src/views/home/Type.vue
+++ yiyi-vue/src/views/home/Type.vue
@@ -1,13 +1,33 @@
 <template>
   <h1>分类</h1>
   <div>
-
+    <CommodityCard :child="commodityList"/>
   </div>
 </template>
 
 <script>
+import CommodityCard from "@/components/home/CommodityCard";
+import axios from "axios";
 export default {
-  name: "Type"
+  name: "Type",
+  components: {CommodityCard},
+  data(){
+    return{
+      commodityList: [],
+    }
+  },
+  created() {
+    this.getCommodityByType()
+  },
+  methods:{
+    // 分类获取商品信息
+    getCommodityByType(){
+      axios.get("type/queryAllCommodityByTypeName",{params:{type:'1'}}).then(res=>{
+        console.log(res)
+      })
+      console.log("分类获取商品信息")
+    }
+  },
 }
 </script>
 
