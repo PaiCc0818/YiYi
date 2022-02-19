34cd913e63b53f34547a361df6f9cec4fcb1e239
ershuaili <liershuai.cn@gmail.com>
2022-02-19 23:14:42 +0800

    购买商品
    Signed-off-by: ershuaili <liershuai.cn@gmail.com>

--- yiyi-vue/src/views/home/Release.vue
+++ yiyi-vue/src/views/home/Release.vue
@@ -5,13 +5,37 @@
     <div>商品详情：<textarea/></div>
     <div>商品分类：<input type="text"></div>
     <div>添加图片：<input type="text"></div>
+    <div><input accept="image/png,image/gif,image/jpeg" name="file" type="file" @change="update"/></div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Release",
-  methods: {}
+  methods: {
+    update(e) {  // 上传照片
+      const self = this;
+      let file = e.target.files[0]
+      /* eslint-disable no-undef */
+      let param = new FormData() // 创建form对象
+      param.append('file', file, file.name) // 通过append向form对象添加数据
+      param.append('chunk', '0') // 添加form表单中其他数据
+      console.log(param.get('file')) // FormData私有类对象，访问不到，可以通过get判断值是否传进去
+      let config = {
+        headers: {'Content-Type': 'multipart/form-data'}
+      }
+      // 添加请求头
+      axios.post('http://172.19.26.60:8081/rest/user/headurl', param, config)
+          .then(response => {
+            if (response.data.code === 0) {
+              self.ImgUrl = response.data.data
+            }
+            console.log(response.data)
+          })
+    }
+  }
 }
 </script>
 
